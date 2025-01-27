86ecfbc036bfafcf28f83b9e2e9e749fc6951864
obsidian <126514631+OBSIDAN55@users.noreply.github.com>
2025-01-27 16:11:12 +0300

    Update trsLiquids.java

--- src/trs/content/trsLiquids.java
+++ src/trs/content/trsLiquids.java
@@ -9,10 +9,16 @@ public class trsLiquids {
 
     public static void load(){
         metan = new Liquid("metan",Color.valueOf("62392CFF")){{
-           gas = true;
+		   	gas = true;
+            barColor = Color.valueOf("");
+            explosiveness = 0.85f;
+            flammability = 0.78f;	   
         }};
         argon = new Liquid("argon", Color.valueOf("263CAFFF")){{
            gas = true;
+            barColor = Color.valueOf("");
+            explosiveness = 0.4f;
+            flammability = 0.65f;
         }};
     }
 }
